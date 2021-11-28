6cf0923899fc0a9ab9d61035c6cf6df56930e4e4
Grant <GrantWils.23@googlemail.com>
2021-11-28 18:31:46 +0000

    add functional make guess code that appends a guess to the guess list and displays results on the next turn

--- run.py
+++ run.py
@@ -32,7 +32,17 @@ class GameBoard():
         for row in self.board_grid:
             print("  ".join(row))
 
-    def display_ships(self, list):
+    def guess(self, x, y):
+        self.player.guesses.append((x, y))
+        self.board_grid[x][y] = "X"
+
+        if (x, y) in self.player.ships:
+            self.board_grid[x][y] = "@"
+            return "Hit"
+        else:
+            return "Miss"
+
+    def display_ships(self):
         # if len(self.player.ships) > self.num_of_ships:
         #     print("Error: you cannot add anymore ships to the board")
         # else:
@@ -75,15 +85,58 @@ def add_ships_to_board(users_board):
         # print(ship) # program checker print
         ship_list.append(ship)
         unique_coordinates = set(ship_list)
-        users_board.player.ships = unique_coordinates
-        users_board.display_ships(users_board.player.ships)
+        users_board.player.ships = list(unique_coordinates)
+        users_board.display_ships()
 
-    # print(ship_list)
-    # print(unique_coordinates)
+    print(ship_list)
+    print(unique_coordinates)
+
+
+def make_guess(guessers_board, opponents_board):
+    """
+    prompt the user to make a guess that is stored in the player.guesses list
+    """
+    if guessers_board.player.player_type == "user":
+        print("please enter the coordinates you would like to strike")
+        x = input(f"please enter an x co-ordinate of 0 or no greater than {guessers_board.size - 1 }\n")
+        y = input(f"please enter an y co-ordinate of 0 or no greater than {guessers_board.size - 1}\n")
+        opponents_board.guess(int(x), int(y))
+    else:
+        x = random_int(guessers_board.size -1)
+        y = random_int(guessers_board.size -1)
+        opponents_board.guess(int(x), int(y))
+
+# def validate_guess():
 
 
 add_ships_to_board(players_board)
 add_ships_to_board(computers_board)
+
+#----------------------------------------------------------------------------
+print()
+print(f"{player1.player_name}'s Battleship Board")
+print("-" * 40)
+print()
+players_board.display_board()
+print(f"{player2.player_name}'s Battleship Board")
+print("-" * 40)
+computers_board.display_board()
+print()
+#----------------------------------------------------------------------------
+
+print(players_board.player.ships)
+print(computers_board.player.ships)
+print()
+print((1, 0) in players_board.player.ships)
+print((2, 0) in players_board.player.ships)
+print((3, 0) in players_board.player.ships)
+print()
+
+#----------------------------------------------------------------------------
+code_checker_variable = make_guess(players_board, computers_board) 
+code_checker_variable = make_guess(computers_board, players_board) 
+print(code_checker_variable in computers_board.player.ships)
+#----------------------------------------------------------------------------
 
 print()
 print(f"{player1.player_name}'s Battleship Board")
@@ -94,10 +147,3 @@ print(f"{player2.player_name}'s Battleship Board")
 print("-" * 40)
 computers_board.display_board()
 print()
-
-print(players_board.player.ships)
-print(computers_board.player.ships)
-
-print((1,0) in players_board.player.ships)
-print((2,0) in players_board.player.ships)
-print((3,0) in players_board.player.ships)
