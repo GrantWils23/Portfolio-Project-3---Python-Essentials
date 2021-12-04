163763ebef0c8c6ee4644a3406ff43e209a64b4c
Grant <GrantWils.23@googlemail.com>
2021-12-04 16:56:20 +0000

    rewrite the code so that it appends the score of the player under their name

--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 from os import sched_setscheduler, strerror
 import random
 
-scores = {"player": 0, "computer": 0}
+scores = {"computer": 0}
 
 
 class CustomError(Exception):
@@ -134,7 +134,7 @@ def validate_input_coordinates(gameboard):
 def calculate_score(turn, gameboard):
     if turn == "Hit":
         if gameboard.player_type == "user":
-            scores["player"] += 1
+            scores[gameboard.player_name] += 1
             return scores
         else:
             scores["computer"] += 1
@@ -159,14 +159,12 @@ def playgame(players_board, computers_board):
     rand_x = int(random_number(players_board.size))
     rand_y = int(random_number(players_board.size))
     computers_turn = make_guess(players_board, rand_x, rand_y)
-    print()
     print(f"Shot fired!, target '{computers_turn}' at co-ordinates{(x, y)}")
     print()
 
     print(f"{players_board.player_name}'s Battleship Board")
     print("-" * 40)
     players_board.display_board()
-    print()
     print(f"{computers_board.player_name}'s Battleship Board")
     print("-" * 40)
     computers_board.display_board()
@@ -181,38 +179,38 @@ def playgame(players_board, computers_board):
 
 #------------------------------------------------------------------------------------------------------
 
-    print(f"{players_board.player_name}. It is your turn to attack!")
-    print("Prepare to enter the grid co-ordinates to strike.")
-    print("-" * 65)
-    players_turn, x, y = validate_input_coordinates(computers_board)
-    print("-" * 65)
-    print(f"Shot fired!, target '{players_turn}' at co-ordinates{(x, y)}")
-    print("-" * 65)
-    print(f"It's now the {computers_board.player_name}'s turn to attack.")
-    rand_x = int(random_number(players_board.size))
-    rand_y = int(random_number(players_board.size))
-    computers_turn = make_guess(players_board, rand_x, rand_y)
-    print(f"Shot fired!, target '{computers_turn}' at co-ordinates{(x, y)}")
-    print()
+    # print(f"{players_board.player_name}. It is your turn to attack!")
+    # print("Prepare to enter the grid co-ordinates to strike.")
+    # print("-" * 65)
+    # players_turn, x, y = validate_input_coordinates(computers_board)
+    # print("-" * 65)
+    # print(f"Shot fired!, target '{players_turn}' at co-ordinates{(x, y)}")
+    # print("-" * 65)
+    # print(f"It's now the {computers_board.player_name}'s turn to attack.")
+    # rand_x = int(random_number(players_board.size))
+    # rand_y = int(random_number(players_board.size))
+    # computers_turn = make_guess(players_board, rand_x, rand_y)
+    # print(f"Shot fired!, target '{computers_turn}' at co-ordinates{(x, y)}")
+    # print()
 
 
-    print(f"{players_board.player_name}'s Battleship Board")
-    print("-" * 40)
-    print()
-    players_board.display_board()
-    print()
-    print(f"{computers_board.player_name}'s Battleship Board")
-    print()
-    print("-" * 40)
-    computers_board.display_board()
-    print()
+    # print(f"{players_board.player_name}'s Battleship Board")
+    # print("-" * 40)
+    # print()
+    # players_board.display_board()
+    # print()
+    # print(f"{computers_board.player_name}'s Battleship Board")
+    # print()
+    # print("-" * 40)
+    # computers_board.display_board()
+    # print()
 
 
-    calculate_score(players_turn, players_board)
-    calculate_score(computers_turn, computers_board)
-    print("The scores at the end of the round are:")
-    print(scores)
-    print()
+    # calculate_score(players_turn, players_board)
+    # calculate_score(computers_turn, computers_board)
+    # print("The scores at the end of the round are:")
+    # print(scores)
+    # print()
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------
@@ -223,11 +221,12 @@ def start_game():
     runs the game and instatiates all the variables required to run the game
     """
 
-    scores["computer"] = 0
-    scores["player"] = 0
+
     board_size = 5
     num_of_ships = 4
     players_name = input("What is your name? \n")
+    scores["computer"] = 0
+    scores[players_name] = 0
     players_board = GameBoard(board_size, num_of_ships, player_type="user", player_name=players_name)
     computers_board = GameBoard(board_size, num_of_ships, player_type="computer", player_name="Computer")
     
