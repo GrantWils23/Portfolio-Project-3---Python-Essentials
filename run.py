35c55613a5f105acefeb0724676ece13110b413f
Grant <GrantWils.23@googlemail.com>
2021-12-07 18:23:14 +0000

    add /n on the end any inputs in the program0

--- run.py
+++ run.py
@@ -74,15 +74,12 @@ def add_ships_to_board(gameboard):
         gameboard.ships = list(unique_coordinates)
         gameboard.display_ships()
 
-    # print(ship_list)
-    # print(unique_coordinates)
-
 
 def input_coordinate(boardsize, row_or_column):
     """
     input a coordinate
     """
-    num = int(input(f'Enter a number between 0 and {boardsize - 1} for the {row_or_column}:  '))
+    num = int(input(f'Enter a number between 0 and {boardsize - 1} for the {row_or_column}:  \n'))
     return num
 
 
@@ -104,10 +101,6 @@ def validate_input(gameboard, row_or_column):
         except ValueError as error:
             e = str(error).split()
             print(f'{e[-1]} is not a number')
-
-##################################################################
-
-#################################################################
 
 
 def validate_input_coordinates(gameboard):
@@ -134,9 +127,6 @@ def validate_input_coordinates(gameboard):
         except CustomError:
             if gameboard.player_type == "computer":
                 print("please select a co-ordinate that hasn't already been chosen")
-                
-
-################################################################
 
 
 def calculate_score(turn, gameboard):
@@ -150,8 +140,6 @@ def calculate_score(turn, gameboard):
         else:
             scores["computer"] += 1
             return scores
-
-################################################################
 
 
 def playgame(players_board, computers_board):
@@ -197,42 +185,6 @@ def playgame(players_board, computers_board):
         scores.pop(players_board.player_name)
         start_game()
     
-
-
-#------------------------------------------------------------------------------------------------------
-
-    # print(f"{players_board.player_name}. It is your turn to attack!")
-    # print("Prepare to enter the grid co-ordinates to strike.")
-    # print("-" * 65)
-    # players_turn, x, y = validate_input_coordinates(computers_board)
-    # print("-" * 65)
-    # print(f"Shot fired!, target '{players_turn}' at co-ordinates{(x, y)}")
-    # print("-" * 65)
-    # print(f"It's now the {computers_board.player_name}'s turn to attack.")
-    # rand_x = int(random_number(players_board.size))
-    # rand_y = int(random_number(players_board.size))
-    # computers_turn = make_guess(players_board, rand_x, rand_y)
-    # print(f"Shot fired!, target '{computers_turn}' at co-ordinates{(x, y)}")
-    # print()
-
-
-    # print(f"{players_board.player_name}'s Battleship Board")
-    # print("-" * 40)
-    # print()
-    # players_board.display_board()
-    # print()
-    # print(f"{computers_board.player_name}'s Battleship Board")
-    # print()
-    # print("-" * 40)
-    # computers_board.display_board()
-    # print()
-
-    # calculate_score(players_turn, players_board)
-    # calculate_score(computers_turn, computers_board)
-    # print("The scores at the end of the round are:")
-    # print(scores)
-    # print()
-
 #--------------------------------------------------------------------------------------------------------------------------------------
 
 def start_game():
